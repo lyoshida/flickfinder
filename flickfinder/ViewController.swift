923be1e6a3c07358b8e8e1e891dd5b7773174fda
Luis Yoshida <luis.yoshida@gmail.com>
2015-07-22 12:01:44 -0300

    Json parsing.

--- flickfinder/ViewController.swift
+++ flickfinder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -26,7 +27,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var longitudeTextField: UITextField!
     @IBOutlet weak var photoTitleLabel: UILabel!
 
+    
+    
     @IBAction func searchPhotosByPhraseButtonTouchUp(sender: UIButton) {
+        
+        let url:String = "https://api.flickr.com/services/rest/?" +
+            "method=flickr.photos.search&" +
+            "api_key=3f859bb8101d02cc700952b1d4e215e6&" +
+            "text=baby+asian+elephant&" +
+            "format=json&" +
+            "nojsoncallback=1"
+        
+        var request = NSURLRequest(URL: NSURL(string: url)!)
+        
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithRequest(request) {
+            (data, response, downloadError) -> Void in
+            
+            if let error = downloadError {
+                print("Could not complete the request \(error)")
+            } else {
+                var parsingError: NSError? = nil
+                do {
+                    let parsedResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
+                    print(parsedResult)
+
+                } catch _ {}
+                
+                    
+                
+            }
+        }
+        
+        task!.resume()
+        
+        /*
+        https://api.flickr.com/services/rest/?
+        method=flickr.photos.search&
+        api_key=df52b9fa8e1632a530e801c286f20f7c&
+        text=baby+asian+elephant&
+        format=json&
+        nojsoncallback=1
+        */
     }
     
     @IBAction func searchPhotosByLatLonButtonTouchUp(sender: UIButton) {
